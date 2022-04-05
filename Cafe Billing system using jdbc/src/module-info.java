d785a0b059a05f93c80098faf54552e7a3bfcfe8
Mahesh B <61156481+mahiib@users.noreply.github.com>
2022-04-05 18:20:02 +0530

    Delete Cafe Billing system using jdbc directory

--- Cafe Billing system using jdbc/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module JavaLA {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.base;
-	requires java.sql;
-	opens application to javafx.graphics, javafx.fxml;
-}
